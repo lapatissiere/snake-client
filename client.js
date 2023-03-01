6470bd581aa670c625a1b9f74c99212a00b1ff99
Nazia Farooq <lapatissiere@yahoo.com>
2023-03-01 15:37:39 -0500

    snake initials completed

--- client.js
+++ client.js
@@ -5,8 +5,14 @@ const connect = function () {
     port: '50541',
   });
   conn.on("connect", () => {
-    console.log("you ded cuz you idled"); // code that does something when the connection is first established
+    console.log("Successfully connected to game server"); // code that does something when the connection is first established
+    conn.write("Name: NFP")
   });
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data); // code that does something when the connection is first established
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
--- play.js
+++ play.js
@@ -1,6 +1,6 @@
 const connect = require("./client")
 
 // establishes a connection with the game server
-
 console.log("Connecting ...");
 connect();
+
