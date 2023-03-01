d1f9774bd552890c34f69766008704d5ee78cec3
Nazia Farooq <lapatissiere@yahoo.com>
2023-03-01 14:59:36 -0500

    exported function from client to play files

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
+const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
+    host: '192.168.141.196',
+    port: '50541',
   });
   conn.on("connect", () => {
     console.log("you ded cuz you idled"); // code that does something when the connection is first established
--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require("net");
+const connect = require("./client")
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.141.196',
-    port: '50541',
-  });
-  conn.on("connect", () => {
-    console.log("you ded cuz you idled"); // code that does something when the connection is first established
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
