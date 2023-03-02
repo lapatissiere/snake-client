6f5adf28cb7fd530e2d151ff75c830f6cccfcdd4
Nazia Farooq <lapatissiere@yahoo.com>
2023-03-01 20:31:22 -0500

    Move up, set tiemout, set interval coded

--- client.js
+++ client.js
@@ -1,13 +1,28 @@
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
-    host: '192.168.141.196',
-    port: '50541',
+    host: "192.168.141.196",
+    port: "50541",
   });
   conn.on("connect", () => {
     console.log("Successfully connected to game server"); // code that does something when the connection is first established
-    conn.write("Name: NFP")
-  });
+    conn.write("Name: NFP");
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 80);
+  //   let count = 0;
+  //   const Move = setInterval(() => {
+  //     count++;
+  //     conn.write("Move: up");
+  //     if (count === 10) {
+  //       clearInterval(Move);
+  //     }
+  //   }, 50);
+  // });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data); // code that does something when the connection is first established
@@ -19,4 +34,4 @@ const connect = function () {
   return conn;
 };
 
-module.exports = connect
+module.exports = connect;
