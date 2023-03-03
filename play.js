91b9d102f95e217f60d3a1fc67919444b764368e
Nazia Farooq <lapatissiere@yahoo.com>
2023-03-02 19:16:00 -0500

    formatted code

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-const connect = require("./client")
+const connect = require("./client");
 
 // establishes a connection with the game server
 console.log("Connecting ...");
@@ -10,18 +10,19 @@ const setupInput = function () {
   stdin.setEncoding("utf8");
   stdin.resume();
   const handleUserInput = function () {
-    process.stdin.on('data', (key) => {
-    //   if (key === '\u0003') {
-    //     process.exit();
-    //   }
-    //   process.stdout.write(key);
-    // });
-    // console.log(after callback);
+    process.stdin.on("data", (key) => {
+      //   if (key === '\u0003') {
+      //     process.exit();
+      //   }
+      //   process.stdout.write(key);
+      // });
+      // console.log(after callback);
+    });
+    process.on("SIGINT", () => {
+      process.exit();
+    });
+
+    stdin.on("data", handleUserInput);
+    return stdin;
   };
-  process.on('SIGINT', () => {
-    process.exit();
-  });
-  
-  stdin.on("data", handleUserInput);
-  return stdin;
 };
